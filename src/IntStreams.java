0b669b4b8f9eb0a805fd8bf4da0cc0fd191f0d9b
cassiano da silva carvalho <cassianosdcarvalho@gmail.com>
2021-03-21 22:42:28 -0300

    repositorio criado

---
+++ src/IntStreams.java
@@ -0,0 +1,11 @@
+import java.util.stream.Stream;
+
+public class IntStreams {
+    public static void main(String[] args) {
+
+                int n= 0;
+               //Stream.of(1,2,3,4,5).forEach(2 ->){
+            System.out.println(n);
+        };
+    }
+}
